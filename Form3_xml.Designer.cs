7505466e60ebeaad45ab98bafac6c55b1a356ecf
yuting <yuting09120310@gmail.com>
2022-03-27 17:21:46 +0800

    xml讀取

---
+++ Form3_xml.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace obiz_load_data
+{
+    partial class Form3_xml
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Add_Text = new System.Windows.Forms.Button();
+            this.Tb_Text = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(18, 105);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(501, 441);
+            this.textBox2.TabIndex = 8;
+            // 
+            // Add_Text
+            // 
+            this.Add_Text.Location = new System.Drawing.Point(384, 45);
+            this.Add_Text.Name = "Add_Text";
+            this.Add_Text.Size = new System.Drawing.Size(75, 23);
+            this.Add_Text.TabIndex = 7;
+            this.Add_Text.Text = "新增";
+            this.Add_Text.UseVisualStyleBackColor = true;
+            // 
+            // Tb_Text
+            // 
+            this.Tb_Text.Location = new System.Drawing.Point(98, 45);
+            this.Tb_Text.Name = "Tb_Text";
+            this.Tb_Text.Size = new System.Drawing.Size(259, 22);
+            this.Tb_Text.TabIndex = 6;
+            // 
+            // Form3_xml
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 568);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Add_Text);
+            this.Controls.Add(this.Tb_Text);
+            this.Name = "Form3_xml";
+            this.Text = "Form3_xml";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Add_Text;
+        private System.Windows.Forms.TextBox Tb_Text;
+    }
+}
---
+++ Form3_xml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace obiz_load_data
+{
+    public partial class Form3_xml : Form
+    {
+        string path = @"E:\123.xml";
+        public Form3_xml()
+        {
+            InitializeComponent();
+            Read_Xml();
+        }
+
+        public void Read_Xml()
+        {
+            XmlDocument myXml = new XmlDocument();
+            myXml.Load(path);
+            XmlNode user_info = myXml.DocumentElement;//讀取XML的根節點
+            foreach (XmlNode node in user_info.ChildNodes)//對子節點進行迴圈
+            {
+                //如果節點名為Address
+                if(node.Name == "Address")
+                {
+                    //將結點內容全部展開
+                    foreach (XmlNode item in node.ChildNodes)
+                    {
+                        //將每個節點的內容顯示出來
+                        switch (item.Name)
+                        {
+                            case "Name":
+                                textBox2.Text += ($"姓名：{item.InnerText}" + Environment.NewLine);
+                                break;
+
+                            case "Country":
+                                textBox2.Text += ($"國家：{item.InnerText}" + Environment.NewLine);
+                                break;
+                        }
+                    }
+                    textBox2.Text += Environment.NewLine;
+                }
+                
+            }
+        }
+    }
+}
